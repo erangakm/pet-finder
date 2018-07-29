95dd8f78710f5215c1f6c9021414dc72d2d4f5c8
Eranga Manchanayake <erangakm@gmail.com>
2018-07-29 14:22:38 +1000

    add first routing url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LostPetReportComponent } from './forms/lost-pet-report/lost-pet-report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'report-lost-pet',
+    component: LostPetReportComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
