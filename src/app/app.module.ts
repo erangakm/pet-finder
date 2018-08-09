afd112039666ac73f3618eb9bf2884bd6fab63b3
Eranga Manchanayake <erangakm@gmail.com>
2018-08-09 21:04:47 +1000

    throwaway commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LostPetReportComponent } from './forms/lost-pet-report/lost-pet-report.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LostPetReportComponent
+    LostPetReportComponent,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatMenuModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
