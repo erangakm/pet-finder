adc8eee4b35594a86698b5a0cf6022d2a01441bc
Eranga Manchanayake <erangakm@gmail.com>
2018-08-21 23:07:42 +1000

    changes to homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,19 @@ import { FooterComponent } from './footer/footer.component';
 import { LostPetReportComponent } from './forms/lost-pet-report/lost-pet-report.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LostPetReportComponent
+    LostPetReportComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
